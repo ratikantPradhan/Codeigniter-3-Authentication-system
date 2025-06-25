7c4e17dc050a955264ecfa0ff89118e37f2eddb8
Ratikanat Pradhan <ratikantapradhan534@gmail.comgit config --global user.email ratikantapradhan534@gmail.com>
2025-06-25 13:39:53 +0530

    update: User model by add update status of user

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -12,10 +12,24 @@ class User_model extends CI_Model
     //     return FALSE;
     // }
 
-
+    
+        public function get_all_users() {
+            return $this->db->get('users')->result_array(); // or your actual table
+        }
+    
+    
     public function insert_user($data)
     {
+        $data['created_at'] = date('Y-m-d H:i:s');
         return $this->db->insert('users', $data);
     }
+    
+    public function update_status($id, $status)
+    {
+        $this->db->where('id', $id);
+        return $this->db->update('users', ['status' => $status]);
+    }
+
+
 }
 ?>
